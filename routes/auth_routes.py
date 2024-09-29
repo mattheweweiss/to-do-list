7927463e2439a9ca4dc545e0e3a0572a039ce5e3
Matthew Weiss <mew34254@uga.edu>
2024-09-28 22:13:41 -0400

    Created basic styling for login page

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,6 +1,7 @@
 from services.data_service import get_connection, open_cursor, close_cursor, close_connection
 
-from flask import Blueprint, render_template
+
+from flask import Blueprint, request, render_template, redirect, url_for
 
 
 auth = Blueprint("auth", __name__, template_folder='template')
@@ -9,4 +10,8 @@ auth = Blueprint("auth", __name__, template_folder='template')
 # Route for login page
 @auth.route('/login', methods=["GET", "POST"])
 def login():
+
+    if request.method == "POST":
+        return redirect(url_for("to_do_list.homepage"))
+
     return render_template('login.html')
